16412c29ae0d5363349ce1de2f3cbe3595da5d20
Stampoulis Nikolaos <nickstamp93@gmail.com>
2015-10-10 20:25:27 +0300

    Database Methods
    Implemented:
    insert Routine-Workout
    update Routine-Workout
    delete Routine-Workout
    initial creation of exercises

--- app/src/main/java/com/gymworkoutmate/nickstamp/gymworkoutmate/Data/Contract.java
+++ app/src/main/java/com/gymworkoutmate/nickstamp/gymworkoutmate/Data/Contract.java
@@ -1,6 +1,15 @@
 package com.gymworkoutmate.nickstamp.gymworkoutmate.Data;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+
+import com.gymworkoutmate.nickstamp.gymworkoutmate.Enumeration.EnumEquipment;
+import com.gymworkoutmate.nickstamp.gymworkoutmate.Enumeration.EnumMuscleGroups;
+import com.gymworkoutmate.nickstamp.gymworkoutmate.Model.Exercise;
+import com.gymworkoutmate.nickstamp.gymworkoutmate.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by nickstamp on 10/10/2015.
@@ -96,5 +105,41 @@ public class Contract {
         public static final String COLUMN_ROUTINE = "routineId";
         public static final String COLUMN_DAY = "day_of_week";
     }
+    public static void createExercises(SQLiteDatabase db) {
 
+        ArrayList<Exercise> initExercises = new ArrayList<>();
+        initExercises.add(new Exercise("Bench Press - Medium Grip", R.drawable.bench_press_medium1, R.drawable.bench_press_medium1
+                , EnumMuscleGroups.CHEST, "3-5", 1, EnumEquipment.BARBELL));
+        initExercises.add(new Exercise("Incline Dumbbell Flyes", R.drawable.incline_dumbell_flyes1, R.drawable.incline_dumbell_flyes2
+                , EnumMuscleGroups.CHEST, null, 1, EnumEquipment.DUMBBELL));
+        initExercises.add(new Exercise("Pullups", R.drawable.pullups1, R.drawable.pullups2
+                , EnumMuscleGroups.BACK, "4", 1, EnumEquipment.BODY_ONLY));
+        initExercises.add(new Exercise("Standing Military Press", R.drawable.military_press1, R.drawable.military_press2
+                , EnumMuscleGroups.SHOULDERS, "5", 1, EnumEquipment.BARBELL));
+        initExercises.add(new Exercise("Biceps Curls", R.drawable.barbell_cur1, R.drawable.barbell_cur2
+                , EnumMuscleGroups.BICEPS, null, 0, EnumEquipment.BARBELL));
+        initExercises.add(new Exercise("Incline Hammer Curls", R.drawable.incline_hammer_curls1, R.drawable.incline_hammer_curls2
+                , EnumMuscleGroups.BICEPS, null, 0, EnumEquipment.DUMBBELL));
+        initExercises.add(new Exercise("Concentration Curls", R.drawable.concentration_curls1, R.drawable.concentration_curls2
+                , EnumMuscleGroups.BICEPS, null, 0, EnumEquipment.DUMBBELL));
+        initExercises.add(new Exercise("Bench Dips", R.drawable.bench_dips1, R.drawable.bench_dips2
+                , EnumMuscleGroups.TRICEPS, "1-3", 1, EnumEquipment.BODY_ONLY));
+        initExercises.add(new Exercise("Full Squats", R.drawable.full_squat1, R.drawable.full_squat2
+                , EnumMuscleGroups.LEGS, "2", 1, EnumEquipment.BARBELL));
+        initExercises.add(new Exercise("Incline Crunches", R.drawable.decline_crunch1, R.drawable.decline_crunch2
+                , EnumMuscleGroups.ABS, null, 0, EnumEquipment.BARBELL));
+
+        for (Exercise item : initExercises) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(Contract.Exercises.COLUMN_TITLE, item.getTitle());
+            contentValues.put(Contract.Exercises.COLUMN_IMAGE1, item.getImg1());
+            contentValues.put(Contract.Exercises.COLUMN_IMAGE2, item.getImg2());
+            contentValues.put(Contract.Exercises.COLUMN_MUSCLE, item.getMuscle().getValue());
+            contentValues.put(Contract.Exercises.COLUMN_OTHER_MUSCLES, item.getOther_muscles());
+            contentValues.put(Contract.Exercises.COLUMN_MECHANICS, item.getMechanics());
+            contentValues.put(Exercises.COLUMN_EQUIPMENT, item.getEquipment().getValue());
+
+            db.insert(Contract.Exercises.TABLE_NAME, "null", contentValues);
+        }
+    }
 }
--- app/src/main/java/com/gymworkoutmate/nickstamp/gymworkoutmate/Data/Database.java
+++ app/src/main/java/com/gymworkoutmate/nickstamp/gymworkoutmate/Data/Database.java
@@ -1,5 +1,6 @@
 package com.gymworkoutmate.nickstamp.gymworkoutmate.Data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -46,6 +47,8 @@ public class Database extends SQLiteOpenHelper {
         db.execSQL(Contract.SQL_CREATE_TABLE_EXERCISE_WORKOUTS_CONNECTION);
         db.execSQL(Contract.SQL_CREATE_TABLE_WORKOUTS_ROUTINES_CONNECTION);
 
+        Contract.createExercises(db);
+
     }
 
     @Override
@@ -88,5 +91,62 @@ public class Database extends SQLiteOpenHelper {
         return items;
     }
 
+    public long insert(Routine item) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Contract.Routines.COLUMN_TITLE, item.getTitle());
+        contentValues.put(Contract.Routines.COLUMN_DAYS, item.getDays());
+        contentValues.put(Contract.Routines.COLUMN_TYPE, item.getType().getValue());
+
+        return getWritableDatabase().insert(Contract.Routines.TABLE_NAME, "null", contentValues);
+    }
+
+    public void insert(Workout item) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Contract.Workouts.COLUMN_TITLE, item.getTitle());
+        contentValues.put(Contract.Workouts.COLUMN_MUSCLE, item.getMuscle().getValue());
+        contentValues.put(Contract.Workouts.COLUMN_TYPE, item.getType().getValue());
+
+        getWritableDatabase().insert(Contract.Workouts.TABLE_NAME, "null", contentValues);
+    }
+
+    public void update(Routine item) {
+
+        ContentValues values = new ContentValues();
+        values.put(Contract.Routines.COLUMN_TITLE, item.getTitle());
+        values.put(Contract.Routines.COLUMN_DAYS, item.getDays());
+        values.put(Contract.Routines.COLUMN_TYPE, item.getType().getValue());
+
+        getReadableDatabase().update(Contract.Routines.TABLE_NAME, values, Contract.Routines._ID + " = " + item.getId(), null);
+    }
+
+    public void update(Workout item) {
+
+        ContentValues values = new ContentValues();
+        values.put(Contract.Workouts.COLUMN_TITLE, item.getTitle());
+        values.put(Contract.Workouts.COLUMN_TYPE, item.getType().getValue());
+        values.put(Contract.Workouts.COLUMN_MUSCLE, item.getMuscle().getValue());
+
+        getReadableDatabase().update(Contract.Workouts.TABLE_NAME, values, Contract.Workouts._ID + " = " + item.getId(), null);
+    }
+
+    public void deleteRoutine(int id) {
+
+        // Define 'where' part of query.
+        String selection = Contract.Routines._ID + " = ?";
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = {String.valueOf(id)};
+        // Issue SQL statement.
+        getWritableDatabase().delete(Contract.Routines.TABLE_NAME, selection, selectionArgs);
+    }
+
+    public void deleteWorkouts(int id) {
+
+        // Define 'where' part of query.
+        String selection = Contract.Workouts._ID + " = ?";
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = {String.valueOf(id)};
+        // Issue SQL statement.
+        getWritableDatabase().delete(Contract.Workouts.TABLE_NAME, selection, selectionArgs);
+    }
 
 }
