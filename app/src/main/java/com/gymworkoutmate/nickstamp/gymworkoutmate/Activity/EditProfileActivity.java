e85ef6b791d57359ef24f6420bd49ae58c68ce6e
Stampoulis Nikolaos <nickstamp93@gmail.com>
2015-11-09 18:41:42 +0200

    EditProfileActivity XML

--- app/src/main/java/com/gymworkoutmate/nickstamp/gymworkoutmate/Activity/EditProfileActivity.java
+++ app/src/main/java/com/gymworkoutmate/nickstamp/gymworkoutmate/Activity/EditProfileActivity.java
@@ -1,12 +1,17 @@
 package com.gymworkoutmate.nickstamp.gymworkoutmate.Activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.gymworkoutmate.nickstamp.gymworkoutmate.R;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
-public class EditProfileActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class EditProfileActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,23 @@ public class EditProfileActivity extends AppCompatActivity {
 
         setUpToolbar();
 
+        setUpFab();
+
     }
 
+    public void showDatePickerDialog(View v) {
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.YEAR, 1985);
+        date.set(Calendar.MONTH, Calendar.JANUARY);
+        date.set(Calendar.DAY_OF_MONTH, 1);
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                this,
+                date.get(Calendar.YEAR),
+                date.get(Calendar.MONTH),
+                date.get(Calendar.DAY_OF_MONTH)
+        );
+        dpd.show(getFragmentManager(), "Datepickerdialog");
+    }
 
     private void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -24,4 +44,17 @@ public class EditProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void setUpFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
 }
